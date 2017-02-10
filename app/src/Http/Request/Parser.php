e0aaf67cdb7eb65946fdd2723cb51fd9a4861394
Shaun Hare <shaun.hare@nottingham.ac.uk>
2017-02-10 21:26:22 +0000

    initial setup og request parser

---
+++ app/src/Http/Request/Parser.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shaunhare
+ * Date: 10/02/2017
+ * Time: 17:20
+ */
+
+namespace NottsDigital\Http\Request;
+
+/**
+ * Class Parser
+ * @package NottsDigital\Http\Request
+ */
+class Parser
+{
+    
+    
+    
+}
---
+++ tests/phpunit/Http/Request/ParserTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace NottsDigital\Tests\Http\Request;
+
+/**
+ * Created by PhpStorm.
+ * User: shaunhare
+ * Date: 10/02/2017
+ * Time: 21:23
+ */
+class ParserTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        
+        parent::setup();
+    }
+}
